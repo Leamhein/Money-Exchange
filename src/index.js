5101aef2d284e93dceef6f17a3ca2e8c763fbb51
Vladimir <Leamhein@hotmail.com>
2018-02-21 15:49:58 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+  var answer = {}, //create object with answers (in future)
+  cash = currency,
+  [halfDollar, quarter, dime, nickel, pennie] = [50, 25, 10, 5, 1];
+  function divider (coin, abbr) { //consider whole parts when dividing into a monetary unit
+    var obj = {};
+    obj[abbr] = 0;
+    while (cash >= coin) {
+      cash -= coin;
+      obj[abbr]++;
+    };
+    if (obj[abbr] > 0) { //only values greater than 0 entered in the answer 
+      Object.assign(answer, obj);
+    };
+  };
+  if (cash > 10000) {
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  };
+  divider(halfDollar, "H");
+  divider(quarter, "Q");
+  divider(dime, "D");
+  divider(nickel, "N");
+  divider(pennie, "P");
+
+  return answer;
+};
